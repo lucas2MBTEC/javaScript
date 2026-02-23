df7155bdb80c58cef070b06993a176377e463407
KrackerOs <lucas.zimovski.brasil@escola.pr.gov.br>
2026-02-23 08:21:09 -0300

    Create atividade01.js
    jh

---
+++ componente/atividade01.js
@@ -0,0 +1,13 @@
+/*/javaScript tem 3 tipos de dados o boleano, numerico e a String 
+o numerico é chamado de namber
+ os dados são armazenados em costantes ou variaveis /*/
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+
+const segundoNumero = 2;
+
+const operacao = meuNumero + segundoNumero;
+
+console.log (operacao)
